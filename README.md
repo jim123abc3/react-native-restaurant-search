54e4f4f99f9e49dc976c3ff21c9ee8328c14dc6b
Jim Plimmer <jim123abc3@gmail.com>
2022-12-04 14:16:40 +0000

    finish off simple react-native app, add README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Instructions if you want to run locally
+
+- Install packages with yarn or npm
+- Sign up to a yelp api developer account and get yourself a free api key
+- Replace the text "REPLACE_ME" with your Api Key
+- Download expo on your phone and run React Native app locally! 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
 import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
+
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
 
@@ -15,18 +16,19 @@ const SearchScreen = () => {
   }
 
   return (
-    <View>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage && <Text>{errorMessage}</Text>}
-      <Text>We have found {results.length} results</Text>
-      <ResultsList results={filterResultsByPrice('$')} title='Cost Effective'/>
-      <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier'/>
-      <ResultsList results={filterResultsByPrice('$$$')} title='Big Spender'/>
-    </View>
+      <ScrollView>
+        <ResultsList results={filterResultsByPrice('$')} title='Cost Effective'/>
+        <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier'/>
+        <ResultsList results={filterResultsByPrice('$$$')} title='Big Spender'/>
+      </ScrollView>
+    </>
   )
 }
 
